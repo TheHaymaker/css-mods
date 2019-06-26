a79c497758f054a483f3a2ee3c2d135c2dd6373b
Brett Haymaker <brett.haymaker@gmail.com>
2019-06-25 20:46:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
-function App() {
+const App = () => {
+  const toggleTheme = () => {
+    const root = document.documentElement;
+    const currentTheme = root.style.getPropertyValue('--bgColor').trim();
+
+    if (currentTheme === 'var(--dark)') {
+      root.style.setProperty('--bgColor', 'var(--light)');
+    } else {
+      root.style.setProperty('--bgColor', 'var(--dark)');
+    }
+  };
+
+  useEffect(() => {
+    toggleTheme();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className={style.AppHeader}>
+        <img src={logo} className={style.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={style.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <button onClick={toggleTheme} className={`${style.AppButton} ${style.AppHeader}`}>
+          Toggle Theme
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
